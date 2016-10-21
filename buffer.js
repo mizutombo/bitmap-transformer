fff13a49870b67c8bbe15c9bf953356898487f16
mizutombo <thomas.shultz52@gmail.com>
2016-10-21 15:44:22 -0700

    coded and validated buffer.js and buffer.test.js

---
+++ buffer.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function getFile(bmp, cb) {
+  fs.readFile(bmp, (err, buffer) => {
+    cb(buffer);
+
+  });
+}
+module.exports = getFile;
--- test/buffer.test.js
+++ test/buffer.test.js
@@ -1,23 +1,14 @@
 const fs = require('fs');
 const assert = require('assert');
+const getFile = require('../buffer');
 
 describe('Buffer', () => {
 
-    it('returned from file', done => {
-        fs.readFile('non-palette-bitmap.bmp', (err, buffer) => {
-            assert(buffer instanceof Buffer);
-            for(let i = 0; i < buffer.length; i++){
-                if(i > 1078) {
-                    buffer[i] = 7;
-                }
-            }
-            fs.writeFile('fubarred.bmp', buffer, done);
-        });
-
-    });
-
-    it('makes buffer from string', done => {
-        const buffer = Buffer.from('the quick brown fox', 'ascii');
+    it('returned buffer', done => {
+        getFile('non-palette-bitmap.bmp', buffer => {
+        assert(buffer instanceof Buffer);
         console.log(buffer);
+        done();
+        });
     });
 });
