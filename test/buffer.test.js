8a853a72114a4e43488a9c8e7c90e13e18a3b746
Michael Freeman <mjfreem1@gmail.com>
2016-10-22 14:11:41 -0700

    trying to get functions working

--- test/buffer.test.js
+++ test/buffer.test.js
@@ -1,15 +1,23 @@
-const fs = require('fs');
 const assert = require('assert');
-const getFile = require('../buffer');
+const importedFunc = require('../buffer');
 
 describe('Buffer', () => {
 
     it('returned buffer', done => {
-        getFile('non-palette-bitmap.bmp', buffer => {
+        importedFunc.getFile('non-palette-bitmap.bmp', buffer => {
             assert(buffer instanceof Buffer);
-            console.log(buffer);
+            // console.log(buffer);
             done();
         });
     });
-    
+
+    it('retrives offset info from header', done => {
+        importedFunc.readHeader('non-palette-bitmap.bmp', buffer => {
+            console.log('callback passed');
+            return buffer;
+        });
+    });
+
 });
+
+
