e84a3848017a504e7ce264e7c74527183d6b1d3e
Michael Freeman <mjfreem1@gmail.com>
2016-10-22 16:07:45 -0700

    function change seems to be done

--- buffer.js
+++ buffer.js
@@ -7,28 +7,35 @@ function getFile(bmp, cb) {
 }
 
 function readHeader(buffer, cb) {
+    let buf = buffer;
     let offsetInfo = buffer.slice(10, 14);
     let num = offsetInfo.readUInt8(0);
-    console.log(num);
-    cb(transformFile(num, buffer));
+    console.log('readHeader cd');
+    cb(transformFile(num, buf, cb));
 }
 
-function transformFile(num, buffer, cb) {
-  for(let i = 0; i < buffer.length; i++){
+function transformFile(num, buf, cb) {
+  console.log('transformFile cb');
+  for(let i = 0; i < buf.length; i++){
       if(i > num) {
-        buffer[i] = buffer[i] * 0.9;
-        // buffer[i] = 255 - buffer[i];
+        // buf[i] = buf[i] * 0.9;
+        buf[i] = 255 - buf[i];
         // buffer[i] = 0xff; //needs bigger i increment
         // buffer[i+1] = 0xff; //same
         // buffer[i + 2] = 0xff; //same
       }
   }
-  cb(writeFile(buffer));
+  cb(writeFile(buf));
 }
 
-function writeFile(buffer) {
-  fs.writeFile('modifiedBMP.bmp', buffer);
+function writeFile(buf) {
+  console.log('writeFile cb');
+  fs.writeFile('modifiedBMP.bmp', buf);
 }
+
+getFile('./non-palette-bitmap.bmp', function(buffer) {
+  buffer;
+});
 // function transformFile
 
 
